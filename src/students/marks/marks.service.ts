c19ce92f99e79e3473aa09e693bd7d08117b99ea
rajchauhan2816 <rajchauhan2816@gmail.com>
2022-07-21 22:58:15 +0530

    Add: Highest Score logics

--- src/students/marks/marks.service.ts
+++ src/students/marks/marks.service.ts
@@ -10,17 +10,39 @@ import { InjectRepository } from '@nestjs/typeorm';
 export class MarksService {
   constructor(
     @InjectRepository(StudentMark)
-    private studentSubjectsRepo: Repository<StudentMark>,
+    private studentMarksRepo: Repository<StudentMark>,
     private studentsService: StudentsService,
     private subjectsService: SubjectsService,
   ) {}
 
   async find(userId: number) {
     const student = await this.studentsService.findOneByUserId(userId);
-    const marks = await this.studentSubjectsRepo.findBy({
+    const marks = await this.studentMarksRepo.findBy({
       student: { id: student.id },
     });
-    return marks;
+    const highestMarks = await this.studentMarksRepo.query(
+      `select * from 
+      (select *  from (SELECT SUM(marks) as total_marks, student_id  FROM student_mark GROUP BY student_id) as x order by x.total_marks desc limit 1)h left join student s on h.student_id = s.id;`,
+    );
+
+    const subjectHighestMarks = await this.studentMarksRepo.query(
+      `select 
+      marks,subject_id , student_id,
+      s.name, s.enrollment_no, s.dob, s.gender,
+      s2.name as subject_name, s2.code as subject_code, s2.description as subject_description
+      from (SELECT student_id , subject_id, marks
+      FROM(
+        SELECT *, ROW_NUMBER()OVER(PARTITION BY subject_id ORDER BY marks DESC) rn
+          FROM student_mark
+      )X
+      WHERE rn = 1)t left join student s on s.id = t.student_id left join subject s2 on s2.id = t.subject_id `,
+    );
+    return {
+      marks,
+      highestMarks: this.formatHighestMarks(highestMarks[0]),
+      subjectHighestMarks:
+        this.mapFormatSubjectHighestMarks(subjectHighestMarks),
+    };
   }
 
   async create(userId: number, createMarksDto: CreateMarksDto) {
@@ -29,7 +51,7 @@ export class MarksService {
     const student = await this.studentsService.findOneByUserId(userId);
     const subject = await this.subjectsService.findOne(subjectId);
 
-    const mark = await this.studentSubjectsRepo.findOneBy({
+    const mark = await this.studentMarksRepo.findOneBy({
       student: { id: student.id },
       subject: { id: subject.id },
     });
@@ -38,12 +60,40 @@ export class MarksService {
       throw new Error('Student already has this subject');
     }
 
-    const newMark = await this.studentSubjectsRepo.save({
+    const newMark = await this.studentMarksRepo.save({
       ...dto,
       student,
       subject,
     });
 
     return newMark;
+  }
+
+  private mapFormatSubjectHighestMarks(data: any[]) {
+    return data.map((d) => ({
+      marks: d['marks'],
+      studentId: d['student_id'],
+      name: d['name'],
+      enrollmentNo: d['enrollment_no'],
+      dob: d['dob'],
+      gender: d['gender'],
+      subject: {
+        id: d['subject_id'],
+        name: d['subject_name'],
+        code: d['subject_code'],
+        description: d['subject_description'],
+      },
+    }));
+  }
+
+  private formatHighestMarks(data: any) {
+    return {
+      totalMarks: data['total_marks'],
+      studentId: data['student_id'],
+      name: data['name'],
+      enrollmentNo: data['enrollment_no'],
+      dob: data['dob'],
+      gender: data['gender'],
+    };
   }
 }
